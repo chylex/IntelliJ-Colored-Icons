cc2b96fd0fef7944eb3fa81e8aea4695f8816db5
chylex <contact@chylex.com>
2020-07-19 06:54:43 +0200

    Add colored icons

--- src/com/chylex/intellij/coloredicons/IconPatcher.java
+++ src/com/chylex/intellij/coloredicons/IconPatcher.java
@@ -11,6 +11,106 @@ public class IconPatcher extends IconPathPatcher{
 	private final Map<String, String> iconPaths = new HashMap<>();
 	
 	public IconPatcher(){
+		addPathWithDark("actions/AddMulticaret");
+		addPathWithDark("actions/back");
+		addPathWithDark("actions/buildAutoReloadChanges");
+		addPathWithDark("actions/cancel");
+		addPathWithDark("actions/collapseall");
+		addPathWithDark("actions/download");
+		addPathWithDark("actions/edit");
+		addPathWithDark("actions/editScheme");
+		addPathWithDark("actions/editSource");
+		addPathWithDark("actions/expandall");
+		addPathWithDark("actions/forward");
+		addPathWithDark("actions/inSelection");
+		addPathWithDark("actions/intentionBulbGrey");
+		addPathWithDark("actions/menu-saveall");
+		addPathWithDark("actions/newFolder");
+		addPathWithDark("actions/nextOccurence");
+		addPathWithDark("actions/prettyPrint");
+		addPathWithDark("actions/previousOccurence");
+		addPathWithDark("actions/realIntentionBulb");
+		addPathWithDark("actions/refactoringBulb"); // TODO not working
+		addPathWithDark("actions/refresh");
+		addPathWithDark("actions/RemoveMulticaret");
+		addPathWithDark("actions/rollback");
+		addPathWithDark("actions/splitHorizontally");
+		addPathWithDark("actions/splitVertically");
+		addPathWithDark("actions/swapPanels");
+		addPathWithDark("actions/synchronizeScrolling");
+		addPathWithDark("actions/syncPanels");
+		addPathWithDark("actions/unselectall");
+		addPathWithDark("actions/upload");
+		
+		addPathWithDark("codeStyle/AddNewSectionRule");
+		
+		addPathWithDark("diff/magicResolve");
+		addPathWithDark("diff/magicResolveToolbar");
+		
+		addPathWithDark("general/add");
+		addPathWithDark("general/addJdk");
+		addPathWithDark("general/filter");
+		addPathWithDark("general/layout");
+		addPathWithDark("general/layoutEditorOnly");
+		addPathWithDark("general/layoutEditorPreview");
+		addPathWithDark("general/layoutPreviewOnly");
+		addPathWithDark("general/pin_tab");
+		addPathWithDark("general/print");
+		addPathWithDark("general/remove");
+		addPathWithDark("general/reset");
+		addPathWithDark("general/zoomIn");
+		addPathWithDark("general/zoomOut");
+		
+		addPathWithDark("graph/layout");
+		addPathWithDark("graph/zoomIn");
+		addPathWithDark("graph/zoomOut");
+		
+		addPathWithDark("hierarchy/class");
+		addPathWithDark("hierarchy/subtypes");
+		addPathWithDark("hierarchy/supertypes");
+		
+		addPathWithDark("icons/toolWindowDatabase");
+		
+		addPathWithDark("modules/addExcludedRoot");
+		
+		addPathWithDark("objectBrowser/sortByType");
+		addPathWithDark("objectBrowser/sorted");
+		addPathWithDark("objectBrowser/sortedByUsage");
+		
+		addPathWithDark("runConfigurations/sortbyDuration");
+		
+		addPathWithDark("toolbarDecorator/addBlankLine");
+		addPathWithDark("toolbarDecorator/addClass");
+		addPathWithDark("toolbarDecorator/addFolder");
+		addPathWithDark("toolbarDecorator/addIcon");
+		addPathWithDark("toolbarDecorator/addJira");
+		addPathWithDark("toolbarDecorator/addLink");
+		addPathWithDark("toolbarDecorator/addPattern");
+		addPathWithDark("toolbarDecorator/addRemoteDatasource");
+		addPathWithDark("toolbarDecorator/addYouTrack");
+		addPathWithDark("toolbarDecorator/export");
+		addPathWithDark("toolbarDecorator/import");
+		
+		addPathWithDark("toolwindows/documentation");
+		addPathWithDark("toolwindows/toolWindowBuild");
+		addPathWithDark("toolwindows/toolWindowChanges");
+		addPathWithDark("toolwindows/toolWindowCommit");
+		addPathWithDark("toolwindows/toolWindowCoverage");
+		addPathWithDark("toolwindows/toolWindowDebugger");
+		addPathWithDark("toolwindows/toolWindowFavorites");
+		addPathWithDark("toolwindows/toolWindowFind");
+		addPathWithDark("toolwindows/toolWindowHierarchy");
+		addPathWithDark("toolwindows/toolWindowInspection");
+		addPathWithDark("toolwindows/toolWindowMessages");
+		addPathWithDark("toolwindows/toolWindowModuleDependencies");
+		addPathWithDark("toolwindows/toolWindowProfiler");
+		addPathWithDark("toolwindows/toolWindowProject"); // TODO not working
+		addPathWithDark("toolwindows/toolWindowRun");
+		addPathWithDark("toolwindows/toolWindowServices");
+		addPathWithDark("toolwindows/toolWindowStructure");
+		addPathWithDark("toolwindows/toolWindowTodo");
+		addPathWithDark("toolwindows/webToolWindow");
+		
 		IconLoader.installPathPatcher(this);
 	}
 	
