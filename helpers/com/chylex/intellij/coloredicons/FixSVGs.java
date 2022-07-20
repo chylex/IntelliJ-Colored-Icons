4e6dbfe1da7849eda578a2b46a1eee0fc69f4d61
chylex <contact@chylex.com>
2022-07-20 22:16:09 +0200

    Add color validation to FixSVGs helper

--- helpers/com/chylex/intellij/coloredicons/FixSVGs.java
+++ helpers/com/chylex/intellij/coloredicons/FixSVGs.java
@@ -8,9 +8,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 import static java.util.Map.entry;
 
 final class FixSVGs {
+	
+	private static final File ICONS_FOLDER = new File("./resources/icons");
+	
 	public static void main(final String[] args) throws IOException {
 		final Charset charset = StandardCharsets.UTF_8;
 		final String encoding = charset.name();
@@ -25,7 +29,7 @@ final class FixSVGs {
 		
 		int changedFiles = 0;
 		
-		for (final File file : FileUtils.listFiles(new File("./resources/icons"), new String[]{ "svg" }, true)) {
+		for (final File file : FileUtils.listFiles(ICONS_FOLDER, new String[]{ "svg" }, true)) {
 			boolean hasChanged = false;
 			
 			final List<String> lines = FileUtils.readLines(file, charset);
@@ -61,9 +65,67 @@ final class FixSVGs {
 				System.out.println("Updated file: " + file);
 			}
 			
+			printWarnings(lines, file);
+			
 			FileUtils.writeLines(file, encoding, lines, "\n");
 		}
 		
 		System.out.println("Total updated files: " + changedFiles);
+	}
+	
+	private static final Pattern invalidColorDefinition = Pattern.compile("\\bfill:[^#]");
+	private static final Pattern hexColorDefinition = Pattern.compile("\\bfill:#([0-9a-f]{6});");
+	
+	private static final Set<String> allowedLightColors = Set.of(
+		"b76db7",
+		"6e6e6e",
+		"59a869",
+		"eda200",
+		"389fd6",
+		"db5860"
+	);
+	
+	private static final Set<String> allowedDarkColors = Set.of(
+		"afb1b3",
+		"b066b0",
+		"499c54",
+		"f0a732",
+		"3592c4",
+		"c75450"
+	);
+	
+	private static final Set<String> skipColorCheck = Set.of(
+		"general/addJdk",
+		"modules/addExcludedRoot",
+		"toolbarDecorator/addClass",
+		"toolbarDecorator/addJira"
+	);
+	
+	private static void printWarnings(final List<String> lines, final File file) {
+		final boolean isDark = file.getName().endsWith("_dark.svg");
+		final String relativeFilePath = ICONS_FOLDER.toPath().relativize(file.toPath()).toString().replace('\\', '/');
+		final String suffixlessFilePath = isDark ? relativeFilePath.replace("_dark.svg", "") : relativeFilePath.replace(".svg", "");
+		
+		if (skipColorCheck.contains(suffixlessFilePath)) {
+			return;
+		}
+		
+		final Set<String> validThemeColors = isDark ? allowedDarkColors : allowedLightColors;
+		
+		for (final String line : lines) {
+			if (invalidColorDefinition.matcher(line).find()) {
+				System.out.println("Warning: Invalid color definition in file: " + relativeFilePath);
+				System.out.println("Line: " + line.trim());
+			}
+			
+			hexColorDefinition.matcher(line).results().forEachOrdered(result -> {
+				final String color = result.group(1);
+				
+				if (!validThemeColors.contains(color)) {
+					System.out.println("Warning: Invalid theme color in file: " + relativeFilePath);
+					System.out.println("Line: " + line.trim());
+				}
+			});
+		}
 	}
 }
